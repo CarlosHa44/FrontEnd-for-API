3453e5273f1de1a6cce0f1545481ca3bd494bad8
CarlosHa44 <carlosha176@gmail.com>
2025-08-13 13:22:30 -0500

    fix:waza

--- src/app/core/environment/environment.prod.ts
+++ src/app/core/environment/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   apiUrl: {
-    products: 'productsmicroservice-production-fde8.up.railway.app',
-    categories: 'categoryservice-production.up.railway.app'
+    products: 'https://productsmicroservice-production-fde8.up.railway.app/api/products',
+    categories: 'https://categoryservice-production.up.railway.app/api/categories'
   }
 };
